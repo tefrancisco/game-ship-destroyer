f59a1d5be8fae5064348062d0a6f102c00f630c1
tefrancisco <dzfranciscodz@gmail.com>
2025-03-09 17:31:05 -0300

    Implementação tamanho aleatório da nave ao spawnar

--- game.js
+++ game.js
@@ -1,33 +1,50 @@
 // Pega a altura e largura da janela do browser
 
-let altura = window.innerHeight
-let largura = window.innerWidth
+let altura = window.innerHeight;
+let largura = window.innerWidth;
 
 // Ao mudar o tamanho da janela, redeclarar as variáveis
 function ajustaTamanhoJogo() {
-    altura = window.innerHeight
-    largura = window.innerWidth
-    console.log(largura, altura)
+  altura = window.innerHeight;
+  largura = window.innerWidth;
+  console.log(largura, altura);
 }
 
-ajustaTamanhoJogo()
+ajustaTamanhoJogo();
 
-// Gerando posição aleatória para spawn da nave
-let posicaoX = Math.floor(Math.random() * largura) - 90
-let posicaoY = Math.floor(Math.random() * altura) - 90
+  // Gerando posição aleatória para spawn da nave
+  let posicaoX = Math.floor(Math.random() * largura) - 90;
+  let posicaoY = Math.floor(Math.random() * altura) - 90;
 
-// Se for zero, não subtrai 90 e continua 0, se não, mantém o mesmo valor
-posicaoX = posicaoX < 0 ? 0 : posicaoX
-posicaoY = posicaoY < 0 ? 0 : posicaoY
+  // Se for zero, não subtrai 90 e continua 0, se não, mantém o mesmo valor
+  posicaoX = posicaoX < 0 ? 0 : posicaoX;
+  posicaoY = posicaoY < 0 ? 0 : posicaoY;
 
-console.log(posicaoX, posicaoY)
+  console.log(posicaoX, posicaoY);
 
-// Criando o elemento html
-let nave = document.createElement('img')
-nave.src = "imagens/nave.png"
-nave.className = "nave"
-nave.style.left = posicaoX + 'px'
-nave.style.top = posicaoY + 'px'
-nave.style.position = 'absolute'
+  // Criando o elemento html
+  let nave = document.createElement("img");
+  nave.src = "imagens/nave.png";
+  nave.className = tamanhoAleatorio();
+  nave.style.left = posicaoX + "px";
+  nave.style.top = posicaoY + "px";
+  nave.style.position = "absolute";
 
-document.body.appendChild(nave)
+  document.body.appendChild(nave);
+
+  console.log(tamanhoAleatorio())
+
+
+function tamanhoAleatorio() {
+  let classe = Math.floor(Math.random() * 3);
+  
+// Não precisa de break por conta do return
+  switch(classe) {
+    case 0:
+        return 'nave'
+    case 1: 
+        return 'nave1'
+    case 2:
+        return 'nave2'
+  }
+}
