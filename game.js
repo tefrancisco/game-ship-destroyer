245c1fbd6f2abbe9c14723326621b88f11fb84d1
tefrancisco <dzfranciscodz@gmail.com>
2025-03-09 17:57:40 -0300

    Adcionado perder vidas ao não clicar na nave, e se clicar manter

--- game.js
+++ game.js
@@ -2,6 +2,8 @@
 
 let altura = window.innerHeight;
 let largura = window.innerWidth;
+let vidas = 0
+let coracoes = document.getElementsByClassName('coracoes')
 
 // Ao mudar o tamanho da janela, redeclarar as variáveis
 function ajustaTamanhoJogo() {
@@ -14,7 +16,18 @@ ajustaTamanhoJogo();
 
 function posicaoRandomica() {
   // Remover nave anterior se existe
-  document.getElementById('nave') ? document.getElementById('nave').remove() : {}
+  if(document.getElementById('nave')) {
+    document.getElementById('nave').remove()
+
+    if(vidas > 2) {
+        window.alert('AAAAAAAAAAAAAAA')
+    }
+
+    else {
+        coracoes[vidas].src = 'imagens/coracao_vazio.png'
+        vidas++
+    }
+  } 
 
   // Gerando posição aleatória para spawn da nave
   let posicaoX = Math.floor(Math.random() * largura) - 90;
@@ -34,6 +47,9 @@ function posicaoRandomica() {
   nave.style.top = posicaoY + "px";
   nave.style.position = "absolute";
   nave.id = "nave";
+  nave.onclick = (event) => {
+    event.target.remove()
+  }
 
   document.body.appendChild(nave);
 
@@ -42,7 +58,7 @@ function posicaoRandomica() {
 
 setInterval(() => {
   posicaoRandomica();
-}, 1000);
+}, 2000);
 
 function tamanhoAleatorio() {
   let classe = Math.floor(Math.random() * 3);
